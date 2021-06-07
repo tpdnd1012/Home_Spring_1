c51d2c5ebb9d439fbb6d0f5b3bad9ca17f2303a6
김세웅 <tpdnd1012@naver.com>
2021-06-08 02:19:00 +0900

    2021-06-08

--- src/main/java/spring/domain/Reply/ReplyRepository.java
+++ src/main/java/spring/domain/Reply/ReplyRepository.java
@@ -2,6 +2,10 @@ package spring.domain.Reply;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Long> {
                             // 새로인터페이스 extends JpaRepository< 엔티티명 , id의자료형 >
+
+    List<ReplyEntity> findAllBypostid(Long postid);
 }
